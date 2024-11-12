2a530c18d2bac9d4adb0345a0d31edfc2df7e4ef
Felipe Santos <felipessilva179@gmail.com>
2024-11-12 13:27:06 -0300

    Cadastro sem BCRYPT

--- routes/cadastro.js
+++ routes/cadastro.js
@@ -7,12 +7,16 @@ router.get('/', function(req, res, next) {
   res.render('TelaDeCadastro');
 });
 
-router.post('/addClient', (req, res, next) => {
-  const { nome, email, senha, matricula } = req.body
+router.post('/addClient', async (req, res, next) => {
+  const { matricula, nome, email, senha } = req.body
   try{
-    
+    console.log({matricula, nome, email, senha});
+    const result = await pool.query(
+      "INSERT INTO usuario(matricula, nome, email, senha) VALUES ($1, $2, $3, $4)", [matricula, nome, email, senha]
+    );
+    res.redirect("/sucesso");
   } catch(error) {
-
+    res.status(400).json({ error: error.message });
   }
 });
 
