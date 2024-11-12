e50c0b0c55a35e1c904a22a0c0d4e67a5b6d475d
Felipe Santos <felipessilva179@gmail.com>
2024-11-12 14:16:13 -0300

    Cadastrado com BCRYPT

--- routes/cadastro.js
+++ routes/cadastro.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
-const pool = require('../db/db.js')
+const pool = require('../db/db.js');
+const bcrypt = require('bcrypt');
 
 /* Renderiza a página de login */
 router.get('/', function(req, res, next) {
@@ -11,8 +12,11 @@ router.post('/addClient', async (req, res, next) => {
   const { matricula, nome, email, senha } = req.body
   try{
     console.log({matricula, nome, email, senha});
+    const round = 10;
+    const hashedPassword = await bcrypt.hash(senha, round);
+    console.log({matricula, nome, email, hashedPassword});
     const result = await pool.query(
-      "INSERT INTO usuario(matricula, nome, email, senha) VALUES ($1, $2, $3, $4)", [matricula, nome, email, senha]
+      "INSERT INTO usuario(matricula, nome, email, senha) VALUES ($1, $2, $3, $4)", [matricula, nome, email, hashedPassword]
     );
     res.redirect("/sucesso");
   } catch(error) {
