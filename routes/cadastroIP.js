2504d56e777b35cb8544ee8bcb1e21d5628e9744
YasminFerrari <161326008+Ferrari65@users.noreply.github.com>
2024-11-21 16:41:41 -0300

    cadastroip

--- routes/cadastroIP.js
+++ routes/cadastroIP.js
@@ -3,22 +3,27 @@ var router = express.Router();
 const pool = require('../db/db.js');
 const { format } = require('date-fns');
 
-/* Renderiza a página de cadastro IP */
+// Renderiza a página de cadastro de IP
 router.get('/', function(req, res, next) {
   res.render('cadastroIP');
 });
 
+// Rota para o envio de dados para o cadastro de IP
 router.post('/addIP', async (req, res) => {
-  const { utilizador, matricula, ip } = req.body
+  const { utilizador, matricula, ip } = req.body;
   const dataAtual = format(new Date(), 'yyyy/MM/dd');
-  console.log('teste')
-  try{
-    console.log({ utilizador, matricula, ip, dataAtual })
+  console.log('teste');
+  
+  try {
+    console.log({ utilizador, matricula, ip, dataAtual });
+    
     const result = await pool.query(
-      "INSERT INTO endereco_ip(utilizador, matricula_utilizador, ip, data_registro) VALUES ($1, $2, $3, $4)", [utilizador, matricula, ip, dataAtual]
+      "INSERT INTO endereco_ip(utilizador, matricula_utilizador, ip, data_registro) VALUES ($1, $2, $3, $4)", 
+      [utilizador, matricula, ip, dataAtual]
     );
+    
     res.redirect("/listagem");
-  } catch(error) {
+  } catch (error) {
     res.status(409).json({ error: error.message });
   }
 });
