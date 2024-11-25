b9e9722d1ae1a68c024874c552428192808cd673
YasminFerrari <161326008+Ferrari65@users.noreply.github.com>
2024-11-25 10:38:40 -0300

    atualização validação 10:38 AM

--- routes/cadastroIP.js
+++ routes/cadastroIP.js
@@ -8,16 +8,17 @@ router.get('/', function(req, res, next) {
   res.render('cadastroIP');
 });
 
+// LOGICAS DE VALIDAÇÃO
+
 // Rota para o envio de dados para o cadastro de IP
 router.post('/addIP', async (req, res) => {
   const { utilizador, matricula, ip } = req.body;
   const dataAtual = format(new Date(), 'yyyy/MM/dd');
-  console.log('teste');
-  
+
   try {
     console.log({ utilizador, matricula, ip, dataAtual });
     
-    const result = await pool.query(
+    await pool.query(
       "INSERT INTO endereco_ip(utilizador, matricula_utilizador, ip, data_registro) VALUES ($1, $2, $3, $4)", 
       [utilizador, matricula, ip, dataAtual]
     );
@@ -27,5 +28,37 @@ router.post('/addIP', async (req, res) => {
     res.status(409).json({ error: error.message });
   }
 });
+
+// Rota para autenticação
+router.post('/autentication', async (req, res) => {
+  const { utilizador, matricula, ip } = req.body;
+
+  // VALIDANDO CAMPOS OBRIGATORIOS
+  if (!utilizador || !matricula || !ip) {
+    return res.render('cadastroIP', { 
+      error: 'Todos os campos são obrigatórios.' }
+    )}
+
+    try {
+      const existigIP = await pool.query("SELECT * FROM endereco_ip WHERE ip = $1", [ip]);
+    
+      if (result.rows.length > 0) {
+        return res.render('cadastroIP', {
+          error: 'IP já cadastrado.'
+        });
+      }
+    
+      const ipData = result.rows[0]; 
+      console.log(ipData);
+    
+      res.redirect("/lista");
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  }
+);
+
+  //  VALIDAÇÃO IP JÁ CADASTRADO
+
 
 module.exports = router;
--- routes/lista.js
+++ routes/lista.js
@@ -13,21 +13,6 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.post('/deletar/:id_ip', async (req, res) => {
-  const id_ip = req.params.id_ip
-  try{
-    const deletarQuery = await pool.query('DELETE FROM endereco_ip WHERE id_ip = $1', [id_ip]);
-    const verificar = await pool.query('SELECT * FROM endereco_ip WHERE id_ip = $1', [id_ip]);
-
-    if (verificar.rowCount === 0) {
-      res.status(200).redirect('/lista')
-    } else {
-      res.status(500).send('O cadastro não foi removido, tente novamente!');
-    }
-    
-  } catch(error) {
-    res.status(500).send(`Erro ao excluir o item: ${error.message}`);
-  }
-});
+router.post('/deletar/:id_ip', async (req, res) => {  const id_ip = req.params.id_ip;  try{    const deletarQuery = await pool.query('DELETE FROM endereco_ip WHERE id_ip = $1', [id_ip]);    const verificar = await pool.query('SELECT * FROM endereco_ip WHERE id_ip = $1', [id_ip]);    if (verificar.rowCount === 0) {      res.status(200).redirect('/lista')    } else {      res.status(500).send('O cadastro não foi removido, tente novamente!');    }      } catch(error) {    res.status(500).send(`Erro ao excluir o item: ${error.message}`);  }});
 
 module.exports = router;
